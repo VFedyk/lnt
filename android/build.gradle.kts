4ef686641cf7bc6c16610350f1e49ff21af46f7f
Volodymyr Fedyk <volodymyr.fedyk@gmail.com>
2026-05-29 06:30:55 +0300

    fix: make attempt to fix android builds

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,17 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
