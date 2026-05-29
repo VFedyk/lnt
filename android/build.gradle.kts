967ceb8c1e2242603b89a5531ac04064ae8106f5
Volodymyr Fedyk <volodymyr.fedyk@gmail.com>
2026-05-29 07:09:33 +0300

    fix: make another attempt to fix android builds

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,8 +20,8 @@ subprojects {
 }
 
 subprojects {
-    afterEvaluate {
-        extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+    plugins.withId("com.android.library") {
+        configure<com.android.build.gradle.LibraryExtension> {
             compileOptions {
                 sourceCompatibility = JavaVersion.VERSION_17
                 targetCompatibility = JavaVersion.VERSION_17
